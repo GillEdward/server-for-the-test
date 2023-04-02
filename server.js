a0d6ef50da373ecaa6385566c35080f34c5dd133
GillEdward <1520868589@qq.com>
2023-04-02 11:46:41 +0800

    add server timer

--- server.js
+++ server.js
@@ -7,6 +7,26 @@ var server = require('ws').Server    // 创建一个websocket服务
 var wss = new server({port:3000})
 const io = require('nodejs-websocket')  // 导入nodejs-websocket模块
 
+console.log("Server started")
+setInterval(function(){ //更新玩家位置坐标  // update players' pos
+    for(var key in playerList){
+        switch(playerList[key]["action"]){  // 改变移动中玩家的坐标   // change moving player's pos
+            case "movingLeft":
+                playerList[key]["pos"][0] -= 1
+                break
+            case "movingUp":
+                playerList[key]["pos"][1] += 1
+                break
+            case "movingRight":
+                playerList[key]["pos"][0] += 1
+                break
+            case "movingDown":
+                playerList[key]["pos"][1] -= 1
+                break
+        }
+    }
+}, 20);  // 50Hz
+
 wss.on('connection', function connection(ws) {
     console.log('New Connection')
 
@@ -17,8 +37,8 @@ wss.on('connection', function connection(ws) {
         switch(json["action"]){
             case "login":
                 ws.send(JSON.stringify({"player-id" : "server", "action" : "loadIn", "value" : playerList})) // 向新玩家发送总表    // send new player the playerList that contant all the ID and pos of players
-                playerList[json["player-id"]] = JSON.parse(JSON.stringify(initPos)) // 添加新玩家信息到总表(深拷贝)
-                wss.broadcast(JSON.stringify({"player-id" : json["player-id"], "action" : "newPlayerLogin", "value" : initPos}))    // 广播新玩家的ID和pos // broadcast new player's ID and pos
+                playerList[json["player-id"]] = {"action" : "standing", "pos" : JSON.parse(JSON.stringify(initPos))} // 添加新玩家信息到总表(深拷贝)
+                wss.broadcast(JSON.stringify({"player-id" : json["player-id"], "action" : "newPlayerLogin", "value" : playerList[json["player-id"]]}))    // 广播新玩家的ID和pos // broadcast new player's ID and pos
                 break
             case "logout":
                 wss.broadcast(JSON.stringify({"player-id" : json["player-id"], "action" : "playerLogout"}))    // 广播登出玩家ID  // broadcast the logout player's ID
@@ -27,21 +47,25 @@ wss.on('connection', function connection(ws) {
                 break
             case "moving":
                 if(json["value"] & 0b1000){ // Left
-                    playerList[json["player-id"]][0] -= 1   // 修改服务器本地坐标    // change local pos of the player
+                    playerList[json["player-id"]]["action"] = "movingLeft"   // 修改player动作信息    // change player's action
+                    wss.broadcast(JSON.stringify({"player-id" : json["player-id"], "action" : "moving", "value" : "Left"}))
                 }
                 if(json["value"] & 0b0100){ // Up
-                    playerList[json["player-id"]][1] += 1
+                    playerList[json["player-id"]]["action"] = "movingUp"
+                    wss.broadcast(JSON.stringify({"player-id" : json["player-id"], "action" : "moving", "value" : "Up"}))
                 }
                 if(json["value"] & 0b0010){ // Right
-                    playerList[json["player-id"]][0] += 1
+                    playerList[json["player-id"]]["action"] = "movingRight"
+                    wss.broadcast(JSON.stringify({"player-id" : json["player-id"], "action" : "moving", "value" : "Right"}))
                 }
                 if(json["value"] & 0b0001){ // Down
-                    playerList[json["player-id"]][1] -= 1
+                    playerList[json["player-id"]]["action"] = "movingDown"
+                    wss.broadcast(JSON.stringify({"player-id" : json["player-id"], "action" : "moving", "value" : "Down"}))
                 }
-                wss.broadcast(JSON.stringify(json))
                 break
             case "movementDone":
-                wss.broadcast(JSON.stringify({"player-id" : json["player-id"], "action" : "movementDone", "value" : playerList[json["player-id"]]}))    // 广播停止运动的玩家的标准坐标   // broadcast the player that stop moving, sycn every player's playerList
+                playerList[json["player-id"]]["action"] = "standing"
+                wss.broadcast(JSON.stringify({"player-id" : json["player-id"], "action" : "movementDone", "value" : json["value"]}))    // 广播停止运动的玩家的标准坐标   // broadcast the player that stop moving, sycn every player's playerList
                 break
         }
     })
@@ -55,7 +79,7 @@ wss.on('connection', function connection(ws) {
     })
 })
 
-wss.broadcast = function broadcast(msg) {   // broadcast function
+wss.broadcast = function broadcast(msg) {   // broadcast to all players
     console.log("Broadcasting: " + msg)
     wss.clients.forEach(function each(client) {
         client.send(msg)
